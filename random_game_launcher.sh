c699f4461f1d4413633e4f9ead8d11796e039eed
Nik Outchcunis <outchy@gmail.com>
2022-01-26 13:02:11 -0500

    update

--- random_game_launcher.sh
+++ random_game_launcher.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-# Random Game Launcher v1.0
-# (Non-interactive single console version)
-# by copperkiddx
+# Random Game Launcher v1.0 (Non-interactive version)
+# by copperkiddx <copperkiddx@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# You can download the latest version of this tool from:
+# https://github.com/copperkiddx/random_game_launcher
 
 #=========   USER OPTIONS   =========
 
@@ -124,3 +139,5 @@ TO-DO
 - Check script at https://www.shellcheck.net/
 
 - Interactive menu to select any core?
+
+- Create official github before launch
