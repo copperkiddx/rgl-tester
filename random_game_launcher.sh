7f65517e910a473e6568ad671c413de21f9e2cc6
copperkiddx <outchy@gmail.com>
2022-02-07 14:46:45 -0500

    update

--- random_game_launcher.sh
+++ random_game_launcher.sh
@@ -67,22 +67,22 @@ checkDependencies () {
 }
 
 getFolderSize () { # Find total disk space used by console-specific ROMS only (used for re-scanning purposes)
-    if [ $console == "NES" ]; then
+    if [ $console == "ARCADE" ]; then
+        games_folder_size="`cd /media/fat/_Arcade; du -c --max-depth=1 -- *.mra 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "GAMEBOY" ]; then
+        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gb *.gbc **/*.gb **/*.gbc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "GBA" ]; then
+        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gba **/*.gba 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "Genesis" ]; then
+        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.bin *.gen *.md **/*.bin **/*.gen **/*.md 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "SMS" ]; then
+        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sms **/*.sms 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "NES" ]; then
         games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.nes *.fds **/*.nes **/*.fds 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     elif [ $console == "SNES" ]; then
         games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sfc *.smc **/*.sfc **/*.smc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "Genesis" ]; then
-        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.bin *.gen *.md **/*.bin **/*.gen **/*.md 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "GBA" ]; then
-        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gba **/*.gba 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "GAMEBOY" ]; then
-        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gb *.gbc **/*.gb **/*.gbc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "SMS" ]; then
-        games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sms **/*.sms 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     elif [ $console == "TGFX16" ]; then
         games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.pce **/*.pce 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "ARCADE" ]; then
-        games_folder_size="`cd /media/fat/_Arcade; du -c --max-depth=1 -- *.mra 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     fi
 
     echo $games_folder_size > "games_folder_size_$console.txt"
@@ -101,14 +101,14 @@ launchMenu () {
             --clear \
             --cancel-label "Exit" \
             --menu "Select a console:" $HEIGHT $WIDTH 4 \
-            "1" "NES" \
-            "2" "SNES" \
-            "3" "Genesis" \
-            "4" "GBA" \
-            "5" "GAMEBOY" \
-            "6" "Master System" \
-            "7" "TurboGrafx-16" \
-            "8" "Arcade" \
+            "1" "Arcade" \
+            "2" "Game Boy" \
+            "3" "GBA" \
+            "4" "Genesis" \
+            "5" "Master System" \
+            "6" "NES" \
+            "7" "SNES" \
+            "8" "TurboGrafx-16" \
              2>&1 1>&3)
         exit_status=$?
         exec 3>&-
@@ -130,35 +130,35 @@ launchMenu () {
 
         case $selection in
             1 )
-                console="NES"
+                console="ARCADE"
                 break
                 ;;
             2 )
-                console="SNES"
-                break
-                ;;
-            3 )
-                console="Genesis"
-                break
-                ;;
-            4 )
-                console="GBA"
-                break
-                ;;
-            5 )
                 console="GAMEBOY"
                 break
                 ;;
-            6 )
+            3 )
+                console="GBA"
+                break
+                ;;
+            4 )
+                console="Genesis"
+                break
+                ;;
+            5 )
                 console="SMS"
                 break
                 ;;
+            6 )
+                console="NES"
+                break
+                ;;
             7 )
-                console="TGFX16"
+                console="SNES"
                 break
                 ;;
             8 )
-                console="ARCADE"
+                console="TGFX16"
                 break
                 ;;                
         esac
@@ -178,38 +178,38 @@ loadRandomRom () {
     sleep 2
 
     # load random ROM # https://raw.githubusercontent.com/pocomane/MiSTer_Batch_Control/master/mbc.c
-    if [[ $random_rom_extension == "fds" ]]; then
+    if [[ $random_rom_extension == "bin" ]]; then
+        /media/fat/Scripts/.mister_batch_control/mbc load_rom "MEGADRIVE.BIN" "$random_rom_path"
+    elif [[ $random_rom_extension == "fds" ]]; then
         /media/fat/Scripts/.mister_batch_control/mbc load_rom "NES.FDS" "$random_rom_path"
+    elif [[ $random_rom_extension == "gbc" ]]; then
+        /media/fat/Scripts/.mister_batch_control/mbc load_rom "GAMEBOY.COL" "$random_rom_path"
     elif [[ $random_rom_extension == "gen" ]]; then
         /media/fat/Scripts/.mister_batch_control/mbc load_rom "GENESIS" "$random_rom_path"
     elif [[ $random_rom_extension == "md" ]]; then
         /media/fat/Scripts/.mister_batch_control/mbc load_rom "MEGADRIVE" "$random_rom_path"
-    elif [[ $random_rom_extension == "bin" ]]; then
-        /media/fat/Scripts/.mister_batch_control/mbc load_rom "MEGADRIVE.BIN" "$random_rom_path"
-    elif [[ $random_rom_extension == "gbc" ]]; then
-        /media/fat/Scripts/.mister_batch_control/mbc load_rom "GAMEBOY.COL" "$random_rom_path"
     else
         /media/fat/Scripts/.mister_batch_control/mbc load_rom "$console" "$random_rom_path"
     fi
 }
 
 rescanRoms () {
-    if [ $console == "NES" ]; then
+    if [ $console == "ARCADE" ]; then
+        current_games_folder_size="`cd /media/fat/_Arcade; du -c --max-depth=1 -- *.mra 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "GAMEBOY" ]; then
+        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gb *.gbc **/*.gb **/*.gbc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "GBA" ]; then
+        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gba **/*.gba 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "Genesis" ]; then
+        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.bin *.gen *.md **/*.bin **/*.gen **/*.md 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "SMS" ]; then
+        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sms **/*.sms 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
+    elif [ $console == "NES" ]; then
         current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.nes *.fds **/*.nes **/*.fds 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     elif [ $console == "SNES" ]; then
         current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sfc *.smc **/*.sfc **/*.smc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "Genesis" ]; then
-        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.bin *.gen *.md **/*.bin **/*.gen **/*.md 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "GBA" ]; then
-        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gba **/*.gba 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "GAMEBOY" ]; then
-        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.gb *.gbc **/*.gb **/*.gbc 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "SMS" ]; then
-        current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.sms **/*.sms 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     elif [ $console == "TGFX16" ]; then
         current_games_folder_size="`cd /media/fat/games/$console; du -c --max-depth=999 -- *.pce **/*.pce 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
-    elif [ $console == "ARCADE" ]; then
-        current_games_folder_size="`cd /media/fat/_Arcade; du -c --max-depth=1 -- *.mra 2>/dev/null | awk '$2 == "total" {total += $1} END {print total}'`"
     fi
 
     previous_games_folder_size="`cat games_folder_size_$console.txt`"
@@ -226,22 +226,22 @@ rescanRoms () {
 
 scanRoms () {
     # find all rom files and print them to a file
-        if [ $console == "NES" ]; then
+        if [ $console == "ARCADE" ]; then
+            find "$arcade_games_folder" -maxdepth 1 -name '*.[Mm][Rr][Aa]' -exec ls > "rom_paths_$console.txt" {} \;
+        elif [ $console == "GAMEBOY" ]; then
+            find "$core_games_folder" -iregex '.*\.\(gb\|gbc\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
+        elif [ $console == "GBA" ]; then
+            find "$core_games_folder" -iregex '.*\.\(gba\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
+        elif [ $console == "Genesis" ]; then
+            find "$core_games_folder" -iregex '.*\.\(bin\|gen\|md\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
+        elif [ $console == "SMS" ]; then
+            find "$core_games_folder" -iregex '.*\.\(sms\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
+        elif [ $console == "NES" ]; then
             find "$core_games_folder" -iregex '.*\.\(nes\|fds\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
         elif [ $console == "SNES" ]; then
             find "$core_games_folder" -iregex '.*\.\(sfc\|smc\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
-        elif [ $console == "Genesis" ]; then
-            find "$core_games_folder" -iregex '.*\.\(bin\|gen\|md\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
-        elif [ $console == "GBA" ]; then
-            find "$core_games_folder" -iregex '.*\.\(gba\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
-        elif [ $console == "GAMEBOY" ]; then
-            find "$core_games_folder" -iregex '.*\.\(gb\|gbc\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
-        elif [ $console == "SMS" ]; then
-            find "$core_games_folder" -iregex '.*\.\(sms\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
         elif [ $console == "TGFX16" ]; then
             find "$core_games_folder" -iregex '.*\.\(pce\)$' ! -name '*[Rr][Ee][Aa][Dd][Mm][Ee]*' -exec ls > "rom_paths_$console.txt" {} \;
-        elif [ $console == "ARCADE" ]; then
-            find "$arcade_games_folder" -maxdepth 1 -name '*.[Mm][Rr][Aa]' -exec ls > "rom_paths_$console.txt" {} \;
         fi
 
     # if rom_paths_$console.txt is empty, no ROMS were found, so exit
